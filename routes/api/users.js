5aae9095da13f7bb5a259c0f88e0ad637e9b081d
Ammar <maansaridot@gmail.com>
2019-10-09 17:24:31 +0500

    include password hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys');
-const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
-const models = require('../../models');
+const models = require("../../models");
 const {
   User
 } = models;
@@ -12,23 +13,50 @@ const {
 // @route   Get api/users/test
 // @desc    Test users route
 // @access  Public
-router.get('/test', (req, res) => {
+router.get("/test", (req, res) => {
   res.json({
-    msg: "User API Works"
-  })
+    msg: "User API Works",
+  });
 });
 
 // @route Post api/users/register
 // @desc Signup User
 // @access Public
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   console.log(req.body);
-  User
-    .create(req.body)
-    .then(user => res.json(user))
-    .catch(err => console.log("Registration Error " + err))
+  const errors = {};
+
+  User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    .then(user => {
+      if (user) {
+        console.log(user);
+        errors.email = "Email already exist";
+        return res.status(400).json(errors);
+      } else {
+        console.log(user);
+
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err))
+          });
+        });
+      }
+    })
+    .catch(err => console.log(err));
 });
-
-
 
 module.exports = router;
